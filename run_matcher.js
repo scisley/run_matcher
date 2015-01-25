0bd5cf4cb22f9134555c654866d8359efa5baca3
Steven Isley <steve.c.isley@gmail.com>
2015-01-25 08:31:12 -0700

    Changes nomenclature from way to segment, except when dealing with xml from OSM

--- run_matcher.js
+++ run_matcher.js
@@ -37,46 +37,46 @@ var addOsmToMap = function(target_map, data) {
 	// This is how many nodes got returned in the data set
 	console.log("Node count:" + _.size(nodes));
 	
-	// Create a way object for each way found in the xml, and link the nodes created earlier
-	var ways = {};
+	// Create a segment object for each way found in the xml, and link the nodes created earlier
+	var segments = {};
 	$.map(ways_xml, function(val, i) {
-		var way = {way_id: val.getAttribute("id"), points: [], nodes:[]};
+		var segment = {segment_id: val.getAttribute("id"), points: [], nodes:[]};
 		$.map( val.getElementsByTagName("nd"), function(nd, i) {
 			var nd_id = nd.getAttribute("ref");
-			way.points.push( nodes[nd_id].point );
-			way.nodes.push( nodes[nd_id] );
+			segment.points.push( nodes[nd_id].point );
+			segment.nodes.push( nodes[nd_id] );
 		});
-		ways[val.getAttribute("id")] = way;
+		segments[val.getAttribute("id")] = segment;
 	});
 	
-	// Plot the ways on the map
-	for (id in ways) {
-		var polyline = L.polyline(ways[id].points, {color: rndColor() }).addTo(target_map);
+	// Plot the segments on the map
+	for (id in segments) {
+		var polyline = L.polyline(segments[id].points, {color: rndColor() }).addTo(target_map);
 		polyline.bindPopup("Route " + id + ", bounded by " + polyline.getBounds().toBBoxString());
 	}
 	
-	// For each node, add what ways it is a member of
+	// For each node, add what segments it is a member of
 	for (id in nodes) {
-		nodes[id].ways = _.filter(ways, function(way){ 
-			var matches = _.find(way.nodes, function(nd){ 
+		nodes[id].segments = _.filter(segments, function(segment){ 
+			var matches = _.find(segment.nodes, function(nd){ 
 				return(nd.id === id); });
 			return(typeof  matches != "undefined"); 
 		});
 	}
 	
-	// Add distance information to each way
-	$.map(ways, function(way, i) {
+	// Add distance information to each segment
+	$.map(segments, function(segment, i) {
 		var d = 0;
-		for (var i=1; i<way.points.length; i++) {
-			d += way.points[i].distanceTo(way.points[i-1]);
+		for (var i=1; i<segment.points.length; i++) {
+			d += segment.points[i].distanceTo(segment.points[i-1]);
 		}
-		way.distance = d;
+		segment.distance = d;
 		return(d);
 	});
 	
-	console.log("Number of ways: " + _.size(ways) );
+	console.log("Number of segments: " + _.size(segments) );
 	console.log(nodes);
-	alist = makeAdjacencyList(ways, nodes);
+	alist = makeAdjacencyList(segments, nodes);
 	
 	// For debugging, plot the critical points
 	$.map(alist, function(node) {
@@ -87,22 +87,22 @@ var addOsmToMap = function(target_map, data) {
 	Data.crit = alist;
 };
 
-var makeAdjacencyList = function(ways, nodes) {
+var makeAdjacencyList = function(segments, nodes) {
 /*
 	What I want: A set of critical nodes, we'll call them intersections, and the other critical
 	nodes they connect to. For each critical node, list the other critical nodes it connects to and
 	provide a link to the route it uses to do that, along with the distance.
 
-	I need to generate an adjacency list. If two ways connect at end points, then it's not a 
-	real intersection, but if 3 ways connect at end points then it is. If a node is shared by
-	two ways, with either way including the node at a non-end point, then it's an intersection.
-	any node at the end of a way that is unique is also an intersection (a dead end)
+	I need to generate an adjacency list. If two segments connect at end points, then it's not a 
+	real intersection, but if 3 segments connect at end points then it is. If a node is shared by
+	two segments, with either segment including the node at a non-end point, then it's an intersection.
+	any node at the end of a segment that is unique is also an intersection (a dead end)
 	
 	Steps:
 		* Make a new array of candidate critical points. Start by adding all start
-		  and end points for each way.
+		  and end points for each segment.
 		* Next, add all non-unique remaining points.
-		* For each remaining point, add as neighbours the end node from each way
+		* For each remaining point, add as neighbours the end node from each segment
 		  the node is a member
 		* For each candidate critical point, check if it only has two neighbours.
 		  If so, it is redundant. For each of its two neighbours, remove it and
@@ -110,29 +110,29 @@ var makeAdjacencyList = function(ways, nodes) {
 		  redundant point	
 */
 	
-	// Add nodes that appear in multiple ways (likely intersections)
+	// Add nodes that appear in multiple segments (likely intersections)
 	var crit = _.filter(nodes, function(node) {
-		return node.ways.length > 1;
+		return node.segments.length > 1;
 	});
 	console.log(crit);
 	
-	// Add start and end points of each way to critical point list
-	boundary_nodes = $.map(ways, function(way, i) {
-		return [way.nodes[0], way.nodes[way.nodes.length-1]];
+	// Add start and end points of each segment to critical point list
+	boundary_nodes = $.map(segments, function(segment, i) {
+		return [segment.nodes[0], segment.nodes[segment.nodes.length-1]];
 	});
 	crit = _.union(crit, _.flatten(boundary_nodes));
 	crit = _.uniq(crit, function(item) { return item.id; });
 	console.log(crit);
 	
-	// For each critical point, look at the ways, find itself in the way, then search 
+	// For each critical point, look at the segments, find itself in the segment, then search 
 	// for new critical points either up or down in the list. Those are the neighbours
 	$.map(crit, function(cp) {
 		cp.neighbours = [];
-		$.map(cp.ways, function(way) {
-			var q = _.indexOf(_.pluck(way.nodes, "id"), cp.id);
-			//console.log(cp.id + ":" + way.way_id + ":" + i);
+		$.map(cp.segments, function(segment) {
+			var q = _.indexOf(_.pluck(segment.nodes, "id"), cp.id);
+			//console.log(cp.id + ":" + segment.segment_id + ":" + i);
 			// Look for the first prior critical point
-			var prior = _.first(way.nodes, q);
+			var prior = _.first(segment.nodes, q);
 			for (p in prior.reverse()) {
 				if (Boolean(_.find(crit, prior[p], function(x) { return x.id; }))) {
 					cp.neighbours.push(prior[p]);
@@ -140,7 +140,7 @@ var makeAdjacencyList = function(ways, nodes) {
 				}
 			}
 			// Look for the first latter critical point
-			var latter = _.rest(way.nodes, q+1);
+			var latter = _.rest(segment.nodes, q+1);
 			for (p in latter) {
 				if (Boolean(_.find(crit, latter[p], function(x) { return x.id; }))) {
 					cp.neighbours.push(latter[p]);
@@ -153,8 +153,8 @@ var makeAdjacencyList = function(ways, nodes) {
 	// What does the distribution of neighbour count look like?
 	// _.countBy(Data.crit, function(x) { return x.neighbours.length; })
 	
-/* 	//Grab all the ways and combine them into a big list of node id's
-	all_nodes = _.flatten(_.pluck(ways, "nodes"));	
+/* 	//Grab all the segments and combine them into a big list of node id's
+	all_nodes = _.flatten(_.pluck(segments, "nodes"));	
 	all_nodes = $.map(all_nodes, function(node,i) {
 		return node.id;
 	});
@@ -165,9 +165,9 @@ var makeAdjacencyList = function(ways, nodes) {
 	for (id in node_counts) if (node_counts[id] > 1) crit_nodes.push(id); 
 	console.log(crit_nodes);
 	
-	// Add the beginning and ending node in every way.
-	boundary_nodes = $.map(ways, function(way, i) {
-		return [way.nodes[0].id,way.nodes[way.nodes.length-1].id];
+	// Add the beginning and ending node in every segment.
+	boundary_nodes = $.map(segments, function(segment, i) {
+		return [segment.nodes[0].id,segment.nodes[segment.nodes.length-1].id];
 	});
 	
 	crit_nodes = _.union(crit_nodes, _.flatten(boundary_nodes));
